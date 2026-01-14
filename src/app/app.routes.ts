f2cd4736a9ae05516a431c4934b2d3bf2cbe5fca
maxence guilbot <maxence.guilbot@epitech.eu>
2026-01-14 16:20:03 +0100

    [Feat] : add starter page / front

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { StarterPage } from './starter-page/starter-page';
+
+export const routes: Routes = [
+  { path: 'starter', component: StarterPage }
+  // Ajoutez d'autres routes ici
+];
